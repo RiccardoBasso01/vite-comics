7e7316d05e057575d9cc356e048a3600e5e1f8b4
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-06-29 18:37:02 +0200

    Aggiunta jumbotron

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,6 +8,37 @@ export default {
 
 <template>
     <!-- main -->
+    <div class="jumbotron">
+        <div class="container-xl">
+            <div class="jumbo-content">
+                <h2 class="jumbo-title">The Flash</h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-xl">
+        <h2>Contennuto principale</h2>
+    </div>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+.jumbotron {
+    height: 500px;
+    background-image: url(../assets/img/jumbotron.avif);
+    background-position: center;
+    background-size: cover;
+    box-shadow: 0px -20px 100px 10px black inset;
+}
+
+.jumbo-content {
+    height: 480px;
+    display: flex;
+    align-items: flex-end;
+}
+
+.jumbo-title {
+    color: white;
+    font-size: 40px;
+    margin-bottom: 50px;
+}
+</style>
